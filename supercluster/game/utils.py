b8e72971ef82ba6520a4b033882de07ea9fb7bc1
Fiona Burrows <fiona@myrmidonprocess.com>
2013-01-26 18:33:25 +0000

    whoops

--- supercluster/game/utils.py
+++ supercluster/game/utils.py
@@ -11,8 +11,8 @@ def check_and_advance_all_game_turns():
     It iterates and checks the turn status of all running games.
     It runs itself every minute using a thread.
     """
-    for game in Game.objects.all()):
-        game.check_and_advance_all_game_turns()
+    for game in Game.objects.all():
+        game.check_and_advance_turn()
 
     Timer(60, check_and_advance_all_game_turns).start()
 
